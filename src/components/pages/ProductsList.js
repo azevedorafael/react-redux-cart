c15c1e45adb403e39e59a9e0a93889427b8277a7
Rafael da S.P. de Azevedo <rafaelsilva.fatec@gmail.com>
2017-11-28 20:41:01 -0200

    Connect Redux Store to the React app component

--- src/components/pages/ProductsList.js
+++ src/components/pages/ProductsList.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 class ProductsList extends React.Component{
     render(){
+        const productsList = this.props.products.map(function(arrayProducts){
+            return(
+                <div key={arrayProducts.id}>
+                    <h2>{arrayProducts.title}</h2>
+                    <h2>{arrayProducts.description}</h2>
+                    <h2>{arrayProducts.price}</h2>
+                </div>
+            )
+            });
         return(
             <div>
                 <h1>List of Products for Sale</h1>
+                {productsList}
             </div>
         )
     }
 }
-export default ProductsList;
+function mapStateToProps(state){
+    return{
+        products: state.products.products
+    }
+}
+export default connect(mapStateToProps)(ProductsList);
