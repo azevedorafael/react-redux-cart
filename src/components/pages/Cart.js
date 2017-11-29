ac3d8bf222755cbf1b4a4b8a39c953d3eafac4cc
Rafael da S.P. de Azevedo <rafaelsilva.fatec@gmail.com>
2017-11-29 21:20:51 -0200

    Control Cart total amount

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Panel, Col, Row, Well, Button, Label, ButtonGroup } from 'react-bootstrap';
+import { Modal, Panel, Col, Row, Well, Button, Label, ButtonGroup } from 'react-bootstrap';
 import { bindActionCreators } from 'redux';
 import { deleteCartItem, updateCart } from '../../actions/cartActions';
 
@@ -17,19 +17,34 @@ class Cart extends React.Component {
             })
         // Delete the product from the array
         let cartAfterDelete = [...currentProductToDelete.slice(0, indexToDelete), ...currentProductToDelete.slice(indexToDelete + 1)]
-       
+
         this.props.deleteCartItem(cartAfterDelete);
     }
 
-    onIncrement(_id){
-        this.props.updateCart(_id,1)
+    onIncrement(_id) {
+        this.props.updateCart(_id, 1)
     }
 
-    onDecrement(_id, quantity){
-        if(quantity > 1){
-            this.props.updateCart(_id,-1)
+    onDecrement(_id, quantity) {
+        if (quantity > 1) {
+            this.props.updateCart(_id, -1)
         }
     }
+
+    constructor(){
+        super();
+        this.state = {
+            showModal:false
+        }
+    }
+
+    open(){
+        this.setState({showModal : true})
+    }
+    close(){
+        this.setState({showModal:false})
+    }
+
 
     render() {
         if (this.props.cart[0]) {
@@ -59,26 +74,51 @@ class Cart extends React.Component {
                         </Col>
                         <Col xs={6} sm={4}>
                             <ButtonGroup style={{ minWidth: '300px' }}>
-                                <Button onClick={this.onDecrement.bind(this,arrayCart._id,arrayCart.quantity)} bsStyle="default" bsSize="small">-</Button>
-                                <Button onClick={this.onIncrement.bind(this,arrayCart._id)} bsStyle="default" bsSize="small">+</Button>
+                                <Button onClick={this.onDecrement.bind(this, arrayCart._id, arrayCart.quantity)} bsStyle="default" bsSize="small">-</Button>
+                                <Button onClick={this.onIncrement.bind(this, arrayCart._id)} bsStyle="default" bsSize="small">+</Button>
                                 <span>     </span>
-                                <Button onClick={this.onDelete.bind(this,arrayCart._id)} bsStyle="danger" bsSize="small">DELETE</Button>
+                                <Button onClick={this.onDelete.bind(this, arrayCart._id)} bsStyle="danger" bsSize="small">DELETE</Button>
                             </ButtonGroup>
                         </Col>
                     </Row>
+                    <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Checking Out</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <h6>Thank you!</h6>
+                            <p></p>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Col xs={6}>
+                                <h6>Total: R$ {this.props.totalAmount} </h6>
+                            </Col>
+                            <Button onClick={this.close.bind(this)}>Close</Button>
+                        </Modal.Footer>
+                    </Modal>
                 </Panel>
             )
-        },this)
+        }, this)
         return (
             <Panel header="Cart" bsStyle="primary">
                 {cartItemsList}
-            </Panel>)
+                <Row>
+                    <Col xs={12}>
+                        <h6>Total amount: {this.props.totalAmount}</h6>
+                        <Button onClick={this.open.bind(this)} bsStyle="success" bsSize="small">
+                            Checkout
+                        </Button>
+                    </Col>
+                </Row>
+            </Panel>
+        )
     }
 }
 
 function mapStateToProps(state) {
     return {
-        cart: state.cart.cart
+        cart: state.cart.cart,
+        totalAmount: state.cart.totalAmount
     }
 }
 
@@ -89,4 +129,4 @@ function mapDispatchToProps(dispatch) {
     }, dispatch)
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Cart);
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
--- src/reducers/cartReducers.js
+++ src/reducers/cartReducers.js
@@ -1,14 +1,21 @@
 export function cartReducers(state={cart: []}, action){
     switch(action.type){
         case "ADD_TO_CART":
-        return {cart: [...state, ...action.payload]}
+        return {...state,
+                cart: action.payload,
+                totalAmount: totals(action.payload).amount,
+                totalQuantity: totals(action.payload).qty
+                }
         break;
 
         case "DELETE_CART_ITEM":
-        return {cart: [...state, ...action.payload]}
+        return {...state,
+                cart:action.payload,
+                totalAmount: totals(action.payload).amount,
+                totalQuantity: totals(action.payload).qty
+                }
         break;
 
-        
         case "UPDATE_CART":
                     // Create temp copy of the array of products
                     const currentProductToUpdate = [...state.cart]
@@ -25,9 +32,31 @@ export function cartReducers(state={cart: []}, action){
                     //remove the product from the index and replace
                     let cartUpdate = [...currentProductToUpdate.slice(0, indexToUpdate), newProductToUpdate,
                         ...currentProductToUpdate.slice(indexToUpdate + 1)]
-            
-                    return {...state,cart:cartUpdate}
+
+                    return {
+                        ...state,
+                        cart:cartUpdate,
+                        totalAmount: totals(cartUpdate).amount,
+                        totalQuantity: totals(cartUpdate).qty                        
+                }
         break;
     }
     return state
+}
+
+export function totals(payloadArr){
+
+    const totalAmount = payloadArr.map(function(cartArr){
+        return cartArr.price * cartArr.quantity;
+    }).reduce(function(a,b){
+        return a+b;
+    },0);
+
+    const totalQuantity = payloadArr.map(function(qty){
+        return qty.quantity;
+    }).reduce(function(a,b){
+        return a + b;
+    },0)
+
+    return {amount: totalAmount.toFixed(2),qty: totalQuantity}
 }
